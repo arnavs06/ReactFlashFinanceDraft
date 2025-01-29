d0cd313abc158e72bfec411d0b7a0b5a12316412
arnavs06 <arnavsk1006@gmail.com>
2025-01-29 14:25:34 -0500

    new hero text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import '../styles/index.css';
 const Hero = () => (
   <div className="hero">
     <h1>Master the Interview Process</h1>
-    <p>Your go-to resource for technical questions, valuation methods, and more!</p>
+    <p>Your go-to resource for technical questions, valuation methods, and much more!</p>
   </div>
 );
 
