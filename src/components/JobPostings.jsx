2f8cd6421ff7958a84de7fbad494b48f1dbf3678
arnavs06 <arnavsk1006@gmail.com>
2025-01-30 13:52:42 -0500

    fixed the cards

--- src/components/JobPostings.jsx
+++ src/components/JobPostings.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/index.css'; 
 
 const JobPostings = () => (
   <div className="new-job-postings">
     <h2>Partnered Services</h2>
     <div className="job-card-container">
-      <div className="job-card">
-        <h3>IBInternships.com</h3>
-        <p>Search for new internships to kickstart your career in finance</p>
-        <a href="https://www.ibinternships.com" target="_blank" rel="noopener noreferrer" className="job-link">Visit IBInternships</a>
-      </div>
-      <div className="job-card">
-        <h3>Search Fund Fellows</h3>
-        <p>Develop technical skills and internship experience through our program.</p>
-        <a href="https://www.searchfundfellows.com" target="_blank" rel="noopener noreferrer" className="job-link">Visit Search Fund Fellows</a>
-      </div>
+      <Link to="https://www.ibinternships.com" className="job-card">
+        <div>
+          <h3>IBInternships.com</h3>
+          <p>Search for new internships to kickstart your career in finance</p>
+        </div>
+      </Link>
+      <Link to="https://www.searchfundfellows.com" className="job-card">
+        <div>
+          <h3>Search Fund Fellows</h3>
+          <p>Develop technical skills and internship experience through our program.</p>
+        </div>
+      </Link>
     </div>
   </div>
 );
 
 export default JobPostings;
+
