0cbf09d0868cf9fcac22baa2969844c1b5bd51e4
arnavs06 <arnavsk1006@gmail.com>
2025-01-29 20:25:12 -0500

    navbar2

--- src/pages/Math.jsx
+++ src/pages/Math.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Navbar from '../components/Navbar';
 import Question from '../components/Question';  // Corrected import to 'Question'
 import '../styles/index.css';
 import '../styles/Math.css';
